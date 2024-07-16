82cbe5b8b7673916b69dcc9aeb5b1c6e100a7d6b
elguille2 <guillermo2pacheco@gmail.com>
2024-07-16 16:54:30 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,43 +1,47 @@
 import socket
 
+def handle_request(request):
+    """Parse the HTTP request and return the aproppiate response"""
+    request_line = request.split("\r\n")[0]
+    method, path, version = request_line.split(" ")
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    if path.startswith("/echo/"):
+        message = path[len("/echo/"):]
+        response_body = message.encode("utf-8")
+        response = (
+            b"HTTP/1.1 200 OK\r\n"          # Status Line
+            b"Content-Type: text/plain\r\n"                                             # Headers
+            b"Content-Length: " + str(len(response_body)).encode("utf-8") + b"\r\n"     # Headers
+            b"\r\n" # Empty line
+            + response_body                                                   
+        )
+    elif path == "/":
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+    return response
+
+def start_server(host, port):
+    """Start the HTTP server and handle incoming connections"""
+    server_socket = socket.create_server((host, port), reuse_port=True)
+    print(f"Server started on {host}:{port}")
 
     while True:
         # Accept a connection
         connection, address = server_socket.accept()
-
         # Read the request
         request = connection.recv(1024).decode("utf-8")
         print(f"Received request: {request}")
 
-        # Analyze the request
-        request_line = request.split("\r\n")[0]
-        print(f"Request Line: {request_line}")
-
-        # Extract the path
-        method, path, version = request_line.split(" ")
-        
-
-        # Responses
-        if path .__contains__("/echo/"):
-            message = path.split("/")[2]
-            response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str(len(message.encode("utf-8"))).encode("utf-8") + b"\r\n\r\n" + message.encode("utf-8")
-        elif path == "/":
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-        else:
-            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
-
-        # Send the response 
+        response = handle_request(request)
         connection.sendall(response)
         connection.close()
 
-    
-
+def main():
+    print("Logs from your program will appear here!")
+    start_server("localhost", 4221)
+        
 
 if __name__ == "__main__":
     main()
